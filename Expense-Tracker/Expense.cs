5926b018afa2948cf3ddcbdd67c9bccfbbf802a0
ebrahiemfrancis741 <ebrahiemfrancis741@gmail.com>
2026-01-21 03:53:05 +0200

    Add project files.

---
+++ Expense-Tracker/Expense.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Expense_Tracker
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public double Ammount { get; set; }
+        public DateOnly Date {  get; set; }
+
+
+    }
+}
