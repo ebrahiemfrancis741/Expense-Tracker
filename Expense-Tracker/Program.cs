ceadfe52fab0b6e277757f34858ec88dbb894cbb
ebrahiemfrancis741 <ebrahiemfrancis741@gmail.com>
2026-01-27 23:28:59 +0200

    Add IsValidArgsCount method

--- Expense-Tracker/Program.cs
+++ Expense-Tracker/Program.cs
@@ -1,9 +1,11 @@
 ﻿
+using System.Collections;
+
 namespace Expense_Tracker
 {
     public class ExpenseTrackerApp
     {
-        static void printExpenses(List<Expense> expenseList)
+        private static void PrintExpenses(List<Expense> expenseList)
         {
             foreach (Expense e in expenseList)
             {
@@ -11,25 +13,72 @@ namespace Expense_Tracker
             }
             Console.WriteLine();
         }
+
+        // checks if the correct number of arguments are given for the specified
+        // operation
+        private static bool IsValidArgsCount(string[] args)
+        {
+            if (args.Length >= 1)
+            {
+                switch (args[0])
+                {
+                    case "list":
+                        if (args.Length == 1)
+                        {
+                            return true;
+                        }
+                        break;
+                    case "summary":
+                        if (args.Length == 1)
+                        {
+                            return true;
+                        }
+                        break;
+                    case "add":
+                        if (args.Length == 5)
+                        {
+                            return true;
+                        }
+                        break;
+                    case "update":
+                        if (args.Length == 5)
+                        {
+                            return true;
+                        }
+                        break;
+                    case "delete":
+                        if (args.Length == 3)
+                        {
+                            return true;
+                        }
+                        break;
+                    default:
+                        return false;
+                }
+            }
+            return false;
+        }
+
+
+
         static void Main(string[] args)
         {
             ExpenseTracker expenseTracker = new ExpenseTracker();
             expenseTracker.AddExpense("food", 1000.0);
             expenseTracker.AddExpense("clothes", 2000.0);
             expenseTracker.AddExpense("coffee", 500.0);
-            printExpenses(expenseTracker.Expenses);
+            PrintExpenses(expenseTracker.Expenses);
 
             expenseTracker.DeleteExpense(2);
-            printExpenses(expenseTracker.Expenses);
+            PrintExpenses(expenseTracker.Expenses);
 
             expenseTracker.AddExpense("internet", 500.0);
             expenseTracker.AddExpense("water", 700.0);
-            printExpenses(expenseTracker.Expenses);
+            PrintExpenses(expenseTracker.Expenses);
 
             expenseTracker.UpdateExpense(1, 2000.0);
-            printExpenses(expenseTracker.Expenses);
+            PrintExpenses(expenseTracker.Expenses);
 
-            //Console.WriteLine("Total expenses: $" + expenseTracker.Summary());
             Console.WriteLine("Total expenses: $" + expenseTracker.Summary(1));
         }
     }
